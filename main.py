f467cb694e2c0f622372f2dcef4c1ed1ca06b0ec
Liakot-Ali <liakotali2k18@gmail.com>
2023-05-08 18:33:33 +0600

    comment unnessary things

--- main.py
+++ main.py
@@ -8,28 +8,32 @@ image = cv2.imread("assets/shape.jpg")
 # Convert the color image into gray
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+# cv2.imshow("Gray",gray_image)
+
 _, thresh_image = cv2.threshold(gray_image, 220, 255, cv2.THRESH_BINARY)
+
+# cv2.imshow("Thresh", thresh_image)
 
 # Find the contours of the image
 contours, hierarchy = cv2.findContours(thresh_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+# print(contours)
 
 # Detect the shape and write the shape name in the shape
 for i, contour in enumerate(contours):
     if i == 0:
         continue
-
     epsilon = 0.01 * cv2.arcLength(contour, True)
     approx = cv2.approxPolyDP(contour, epsilon, True)
 
-    cv2.drawContours(image, contour, 0, (0, 0, 0), 4)
+    cv2.drawContours(image, contour, 0, (0, 0, 255), 5)
 
     # Find the coordinates of a specific shape
     x, y, w, h = cv2.boundingRect(approx)
-    x_mid = int(x + (w / 3))
-    y_mid = int(y + (h / 1.5))
+    x_mid = int(x + 10)
+    y_mid = int(y + h + 25)
 
     coords = (x_mid, y_mid)
-    colour = (0, 0, 0)
+    colour = (0, 0, 255)
     font = cv2.FONT_HERSHEY_DUPLEX
 
     # Define the shape
